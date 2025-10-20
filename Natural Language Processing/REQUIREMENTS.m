a8536b7e8aec901631d0636016382ce89c2eeeaf
Saniya Sayyed <sayyedsaniya1010@gmail.com>
2025-10-20 19:52:57 +0530

    Fix formatting for Model section in requirements

--- Natural Language Processing/REQUIREMENTS.m
+++ Natural Language Processing/REQUIREMENTS.m
@@ -2,7 +2,7 @@
 - scispaCy: Extension of spaCy designed for biomedical and scientific text mining
 - PubMed: Open source database to download scientific atricles using API call
 - Pandas: Python's library for data manipulation and analysis
-- *Model*: 
+- **Model**: 
 BioNLP13CG model from SciSpacy (en_ner_bionlpcg_md): best accuracy, large size (~800 mb)
 en_core_sci_sm: smaller, lightweight for exploration
 Link: https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.4.0/en_ner_bionlp13cg_md-0.4.0.tar.gz
